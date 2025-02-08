cd902169377d8bd41c76951e9786c28b3ceac54f
sandeep1555 <sandeepdasari614@gmail.com>
2025-02-08 15:17:09 +0530

    Added httpOnly secure saemsite

--- app.js
+++ app.js
@@ -30,9 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.FE_ORIGIN,
-    credentials: true, 
-     methods: "GET,POST,PUT,DELETE,OPTIONS",
-    allowedHeaders: "Content-Type, Authorization"
+    credentials: true
 }));
 
 
