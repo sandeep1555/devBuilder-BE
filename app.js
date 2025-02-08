3fff1b6e684b693a90a2bf032424eb9bb296a427
sandeep1555 <sandeepdasari614@gmail.com>
2025-02-08 14:59:28 +0530

    added methods in cors

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.FE_ORIGIN,
-    credentials: true
+    credentials: true, 
+     methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization"
 }));
 
 
